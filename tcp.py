38a3404d78f439b52d7e4dab89e8310a1225d7dd
vermillion24 <samwelrandwet@gmail.com>
2026-07-14 15:27:31 -0500

    send update on tcp

--- tcp.py
+++ tcp.py
@@ -3,6 +3,6 @@ target_host = "127.0.0.1"
 target_port = 9999
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((target_host, target_port))
-client.send(b"GET HTTP /1.1 \r\nHost: 127.0.0.1")
+client.send(b"GET / HTTP /1.1\r\nHost: 127.0.0.1\r\n\r\n")
 response = client.recv(4096)
 print(response)
